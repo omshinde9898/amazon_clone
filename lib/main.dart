42d980e9733a467869350a3c11f8e5c752c2913f
OmShinde <omshinde9898@gmail.com>
2023-02-28 20:56:21 +0530

    repo init

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:amazon_clone/constants/global_variables.dart';
+import 'package:amazon_clone/features/auth/screens/auth_screen.dart';
+import 'package:amazon_clone/routes.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Amazon Clone',
+      theme: ThemeData(
+        colorScheme: const ColorScheme.light(
+          primary: GlobalVariables.secondaryColor,
+        ),
+        scaffoldBackgroundColor: GlobalVariables.backgroundColor,
+        appBarTheme: const AppBarTheme(
+          elevation: 0,
+          iconTheme: IconThemeData(color: Colors.black),
+        ),
+      ),
+      onGenerateRoute: (settings) => generateRoute(settings),
+      home: AuthScreen(),
+    );
+  }
+}
