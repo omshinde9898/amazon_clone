f6c80e7fb309b5fdb4dea2794eea952115a2dffd
OmShinde <omshinde9898@gmail.com>
2023-05-04 18:54:58 +0530

    exact server implementation

--- server/index.js
+++ server/index.js
@@ -1,32 +1,34 @@
-console.log("Hello World!");
+// IMPORTS FROM PACKAGES
+const express = require("express");
+const mongoose = require("mongoose");
+const adminRouter = require("./routes/admin");
+// IMPORTS FROM OTHER FILES
+const authRouter = require("./routes/auth");
+const productRouter = require("./routes/product");
+const userRouter = require("./routes/user");
 
-// Import
-const express = require('express');
-const mongoose = require('mongoose');
-const authRouter = require('./routes/auth.js');
-const DB = "DATABASE API";
-
-//INIT
+// INIT
+const PORT = process.env.PORT || 3000;
 const app = express();
-const PORT = 3000;
- 
-//middle ware
-app.use(express.json()); 
+const DB = "mongodb+srv://omshinde:warlock@cluster0.jpvlkwx.mongodb.net/?retryWrites=true&w=majority";
+
+// middleware
+app.use(express.json());
 app.use(authRouter);
+app.use(adminRouter);
+app.use(productRouter);
+app.use(userRouter);
 
-mongoose.connect(DB).then(()=>{
-    console.log('connection succesful');
-}).catch((e)=>{
+// Connections
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log("Connection Successful");
+  })
+  .catch((e) => {
     console.log(e);
-});
+  });
 
-//GET req
-
-app.get("/",(req,res)=>{
-    res.json({greet:"Hello World!"});
-});
-
-
-app.listen(PORT,"0.0.0.0",()=>{
-    console.log(`Connected at Port : ${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`connected at port ${PORT}`);
 });
